af06dc0ff4cc66cedfbcc626838558e0326c53cc
jmednikov <jmednikov13@gmail.com>
2018-11-16 19:44:29 -0800

    Add files via upload

---
+++ HelloLab/src/hellolab/HelloLab.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hellolab;
+
+/**
+ *
+ * @author Frumpkin
+ */
+public class HelloLab {
+
+    
+    public static void main(String[] args) {
+        String x="Jake Mednikov";
+        System.out.println("Hello,my name is Jake Mednikov, and"
+                + " welcome to the hellish world that is java.");
+        System.out.println("this is a test of travis to make sure this works ");
+    }
+    
+}
